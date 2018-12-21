9a3f8b5c651b65d92b3e2f35243ff203d187956f
Owen Stranathan <owen+github@appfigures.com>
2018-12-20 20:32:07 -0500

    Change board size

--- dist/mathwars.js
+++ dist/mathwars.js
@@ -2,8 +2,8 @@
   const SERVER_HOST = "wabisoft.io";
   const SERVER_PORT = "3000";
   const MAX_VALUE = 20;
-  const BOARD_ROWS = 7;
-  const BOARD_COLUMNS = 7;
+  const BOARD_ROWS = 4;
+  const BOARD_COLUMNS = 4;
   const SERVER_ADDR = SERVER_HOST + ":" + SERVER_PORT;
   const BLOCK_SIZE = 80;
   const BLOCK_CENTER = BLOCK_SIZE / 2;
